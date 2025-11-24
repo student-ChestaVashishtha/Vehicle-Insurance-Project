ddab5d5bf368fb9c3443288146cbe5b3a0eea76d
student-ChestaVashishtha <student-ChestaVashishtha>
2025-11-25 00:19:43 +0530

    dding Data_ingestion

---
+++ src/configuration/mongo_db_connection.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import pymongo
+import certifi
+
+from src.exception import MyException
+from src.logger import logging
+from src.constants import DATABASE_NAME, MONGODB_URL_KEY
+
+# Load the certificate authority file to avoid timeout errors when connecting to MongoDBc
+ca=certifi.where()
+
+class MongoDBClient:
+    client=None
+    def __init__(self, database_name: str = DATABASE_NAME) ->None:
+        try:
+            if MongoDBClient.client is None:
+                mongo_db_url=os.getenv(MONGODB_URL_KEY)
+                if mongo_db_url is None:
+                    raise Exception(f"Environment variable '{MONGODB_URL_KEY}' is not  set")
+                # Establish a new MongoDB client connection
+                MongoDBClient.client = pymongo.MongoClient(mongo_db_url,tlsCAFile=ca)
+            self.client=MongoDBClient.client
+            self.database=self.client[database_name]
+            self.database_name=database_name
+            logging.info("MongoDB connection succesful.")
+        except Exception as e:
+            raise MyException(e,sys)
+        
+
+        
